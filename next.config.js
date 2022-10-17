3a21602403fbce9d72ebfb4ba37196b506b7a828
C0rn3l1a <nachoborello97@gmail.com>
2022-10-18 10:47:23 +1100

    Fix Base path

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
-let basePath = '/'
+let assetPrefix = '';
+let basePath = '';
 
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
 }
 
 /** @type {import('next').NextConfig} */
