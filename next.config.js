143a8f1f7ebe4dd80dbc71c63775d7f71bf0f82a
C0rn3l1a <nachoborello97@gmail.com>
2022-10-18 10:35:06 +1100

    Add more gh-pages config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,26 @@
 const path = require('path');
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  },
+  }
 };
 
 module.exports = nextConfig;
